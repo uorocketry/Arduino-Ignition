108f7918d5eeb38948430536430c58c547e4a0e7
Omar Husain <omar.husain2@gmail.com>
2017-10-08 23:38:25 -0400

    initial commit

--- IgnitionDetect/IgnitionDetect.ino
+++ IgnitionDetect/IgnitionDetect.ino
@@ -1,79 +1,107 @@
 #include <Servo.h>
 #include <Arduino.h>
 
-  Servo servo;
+Servo servo;
 
-
-int inPin = 2;         // the number of the input pin
-int interruptPin = 3; // pin that triggers interrupt
 int close_servo = 25;
 int servoPin = 9;
+const int led = 6; // place mat for igniter
 
-boolean interrupted = false;
-boolean ran = false;
-boolean switch_state = false;      // the current state of the output pin
-boolean switch_reading;           // the current reading from the input pin
-boolean switch_prev = true;    // the previous reading from the input pin
+/* Assign states to input commands */ 
+typedef enum States{
+STOP = '1',
+IGNITE = '2',
+OPEN = '3',
+WAIT = '4',
+CLOSE = '5'
+};
 
-long time = 0;         // the last time the output pin was toggled
-long debounce = 200;   // the debounce time, increase if the output flickers
+States state;
+void stateMachine(enum States state);
 
 void setup()
 {
-  pinMode(inPin, INPUT);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), kill_test, LOW);
-      servo.attach(servoPin);
-
+  //attachInterrupt(digitalPinToInterrupt(interruptPin), kill_test, LOW);
+  servo.attach(servoPin);
+  // initial state is stop
+  state = (States)WAIT;
 }
 
 void loop()
 {
-  switch_reading = digitalRead(inPin);
-
-  if (switch_reading != switch_prev && millis() - time > debounce or switch_reading != switch_prev && millis() - time > debounce) {
-    if (switch_state)
-      switch_state = false;
-    else
-      switch_state = true;
-
-    time = millis();
+  /* Read input Commands */
+  if (Serial.available()) {
+    state = (States)Serial.read();
+    Serial.write(state);
+    // TO DO: some check statement if serial input is not a defined enum value
   }
-
-  //**************************** insert code for valve in here***************************************
-  if (switch_state && !ran) {
-    Serial.println("on");
-
-    servo.write(70);
-    delay(2000);
-
-    for (int pos = 70; pos <= 110; pos++) {
-      servo.write(pos);
-      delay(40);
-    }
-
-    for (int pos = 110; pos <= 180; pos++) {
-      servo.write(pos);
-      delay(15);
-    }
-    delay(10000);
-    servo.write(close_servo);
-    delay(500);
-    servo.detach();
-    ran = true;
-  }
-  else {
-    Serial.println("off");
-  }
-  //*************************************************************************************************
-
-  switch_prev = switch_reading;
+  stateMachine(state);
 }
 
+
+/* Emergency Stop Function */
 void kill_test() {
   servo.attach(servoPin);
   Serial.println("END");
   servo.write(close_servo);
   servo.detach();
+}
 
+/*State machine function */
+void stateMachine(enum States state)
+{
+  switch (state){
+   case STOP:
+   // Go to either Ignite, wait
+     digitalWrite(led, LOW);
+     state = WAIT;
+     break;
+   case IGNITE:
+     digitalWrite(led, HIGH);
+     state = WAIT;
+     break;
+   case OPEN:
+     valveOpen();
+   // Go to CLOSE
+   state = CLOSE;
+     break;
+   case CLOSE:
+     valveClose();
+     // Go to OPEN or RESET
+     state = WAIT;
+     break;
+   case WAIT:
+     // delay until some command is given
+     break;
+   default:
+     break;
   }
+}
+
+/* Valve Open Sequence */
+void valveOpen(){
+  
+  servo.write(70);
+  delay(2000);
+  
+  for (int pos = 70; pos <= 110; pos++) {
+    servo.write(pos);
+    delay(40);
+  }
+  
+  for (int pos = 110; pos <= 180; pos++) {
+    servo.write(pos);
+    delay(15);
+  }
+  delay(10000);
+}
+
+/* Valve Closed */
+void valveClose(){
+  servo.write(close_servo);
+  delay(500);
+  servo.detach();
+}
